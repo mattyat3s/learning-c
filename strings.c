49d583c0c73412c6789064e84829b06240829a78
Matthew Yates <matt@Matthews-MBP.home>
2017-01-24 22:52:10 +0000

    Add the following gitignore file strings test file

---
+++ strings.c
@@ -0,0 +1,13 @@
+#import <stdio.h>
+char* string();
+
+int main () {
+  char* x = string();
+
+  printf("Hello %s\n", x);
+  return 0;
+}
+
+char* string() {
+  return "aasdasdasdsdasdasdasdasdasdjhsdfhjshkfskhfskhsdfjhkfsdhjdsbjn";
+}
